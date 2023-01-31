734f49680028a765c742e90295412c786b97af16
Pietro Giordano <giopi87@gmail.com>
2023-01-31 15:05:32 +0100

    esercizio base completo

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,30 @@ createApp({
 
             return {
 
-
+                  email: [],
 
             };
 
       },
 
       methods: {
+
+      },
+
+      mounted() {
             
-            
+            for(let i = 0; i <= 9; i++) {
+
+                  axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+
+                              console.log(response);
+                              this.email.push(response.data.response);
+
+                        });
+
+            }
 
       },
 
